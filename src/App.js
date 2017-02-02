f9993d62bcde3125f55967cbcc4c74f6c3164a71
Joseph Jalbert <joseph.a.jalbert@gmail.com>
2017-02-01 22:00:42 -0500

    initial markup

--- src/App.js
+++ src/App.js
@@ -6,14 +6,41 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <table>
+          <thead>
+            <tr>
+              <th>3:33 3rd</th>
+              <th>1</th>
+              <th>2</th>
+              <th>3</th>
+              <th>4</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="away">
+              <td>
+                <div>img</div>
+                <div>awayteam</div>
+              </td>
+              <td>23</td>
+              <td>11</td>
+              <td>33</td>
+              <td></td>
+            </tr>
+            <tr class="home">
+              <td>
+                <div>img</div>
+                <div>awayteam</div>
+              </td>
+              <td>42</td>
+              <td>13</td>
+              <td>21</td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
       </div>
+      
     );
   }
 }
