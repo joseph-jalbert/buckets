7a4b05fa47c5b4cbddca8a9ae697435cc2c77d13
Joseph Jalbert <joseph.a.jalbert@gmail.com>
2017-02-23 13:15:00 -0500

    parse JSON on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,21 @@ import './App.css';
 class App extends Component {
   state = {scoreboard: []}
   async componentDidMount() {
-    const response = await fetch('/scores')
-    const scoreboard  = await response.json()
-    this.setState({scoreboard: scoreboard});
+    var response = await fetch('/scores')
+    var data  = await response.json()
+    var scores  = JSON.parse(data);
+    this.setState({scoreboard: scores.scoreboard.gameScore});
+    console.log(scores.scoreboard.gameScore);
   }
+      
   render() {
     return (
       <div>
         <ul>
-          {this.state.scoreboard}
+        {this.state.scoreboard.map(event => {
+          return(<li key={event.game.ID}>{event.game.ID}</li>)
+          })
+        }
         </ul>
       </div>
     );
