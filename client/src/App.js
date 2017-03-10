cf110ebc74e6a1e34006870d9ea966e605d3004d
Joseph Jalbert <joseph.a.jalbert@gmail.com>
2017-03-10 18:31:05 -0500

    fixed width tables

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return(
       <div className="main-container">
-        <h1>Just BUCKETS</h1>
+        <h1 className="col-xs-12">Just BUCKETS</h1>
         <ScoreboardContainer/>
       </div>
     );
--- client/src/Game.js
+++ client/src/Game.js
@@ -7,10 +7,10 @@ var GameInfo = require('./GameInfo');
 
 function Game(games){
   return(
-    <div className="container">
+    <div>
         {games.gameInfo.map(event => {
           return(
-						<div className="col-xs-12 col-sm-4 col-md-6" key={event.game.ID}>
+						<div className="col-xs-8 col-sm-6 col-lg-3" key={event.game.ID}>
 	            <table className="table">
 		            <Quarters quarter={event.quarterSummary} />
 		            <QuarterScores game={event} />
