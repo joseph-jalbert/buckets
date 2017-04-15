0ac3e91c8e0f5f9d01e3bd1b93222472591e0ebc
Joseph Jalbert <joseph.a.jalbert@gmail.com>
2017-04-15 18:26:54 -0400

    get playoff data, fix daily games check

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ function getDate() {
   var dateString = yearString + monthString + dayString;
   freshDate = dateString;
   options = {
-    uri: 'https://www.mysportsfeeds.com/api/feed/pull/nba/2016-2017-regular/scoreboard.json?fordate=' + freshDate,
+    uri: 'https://www.mysportsfeeds.com/api/feed/pull/nba/2017-playoff/scoreboard.json?fordate=' + freshDate,
     headers: {
       "Authorization": "Basic " + creds
     }
