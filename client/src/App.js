c872096d8055164fa9c7b23d7d11cc0974fffb83
Joseph Jalbert <joseph.a.jalbert@gmail.com>
2017-05-31 15:45:38 -0400

    add ChooseDate component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import ScoreboardContainer from './ScoreboardContainer';
+import ChooseDate from './DatePicker';
+
 import './App.css';
 
 class App extends Component {
@@ -9,6 +11,7 @@ class App extends Component {
         <header>
           <h1 className="col-xs-12 header">Just BUCKETS</h1>
         </header>
+        <ChooseDate />
         <ScoreboardContainer/>
       </div>
     );
